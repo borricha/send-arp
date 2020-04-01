05d820846cc5b9e0a4ba25183214b1dc5517cd7b
gilgil <gilgil@gilgil.net>
2020-04-02 04:21:59 +0900

    Update usage

--- main.cpp
+++ main.cpp
@@ -11,8 +11,8 @@ struct EthArpPacket {
 #pragma pack(pop)
 
 void usage() {
-    printf("syntax: pcap-test <interface>\n");
-    printf("sample: pcap-test wlan0\n");
+	printf("syntax: send-arp-test <interface>\n");
+	printf("sample: send-arp-test wlan0\n");
 }
 
 int main(int argc, char* argv[]) {
@@ -31,8 +31,8 @@ int main(int argc, char* argv[]) {
 
     EthArpPacket packet;
 
-    packet.eth_.dmac_ = Mac("000000-000000");
-    packet.eth_.smac_ = Mac("000000-000000");
+	packet.eth_.dmac_ = Mac("00:00:00:00:00:00");
+	packet.eth_.smac_ = Mac("00:00:00:00:00:00");
     packet.eth_.type_ = htons(EthHdr::Arp);
 
     packet.arp_.hrd_ = htons(ArpHdr::ETHER);
@@ -40,9 +40,9 @@ int main(int argc, char* argv[]) {
     packet.arp_.hln_ = Mac::SIZE;
     packet.arp_.pln_ = Ip::SIZE;
     packet.arp_.op_ = htons(ArpHdr::Request);
-    packet.arp_.smac_ = Mac("000000-000000");
+	packet.arp_.smac_ = Mac("00:00:00:00:00:00");
     packet.arp_.sip_ = htonl(Ip("0.0.0.0"));
-    packet.arp_.tmac_ = Mac("000000-000000");
+	packet.arp_.tmac_ = Mac("00:00:00:00:00:00");
     packet.arp_.tip_ = htonl(Ip("0.0.0.0"));
 
     int res = pcap_sendpacket(handle, reinterpret_cast<const u_char*>(&packet), sizeof(EthArpPacket));
