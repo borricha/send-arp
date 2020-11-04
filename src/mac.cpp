8b89a6323ccaa356e35dfb17316e0d7618c410d8
gilgil <gilgil@gilgil.net>
2020-11-04 19:47:38 +0900

    Change Mac class

--- src/mac.cpp
+++ src/mac.cpp
@@ -1,66 +1,114 @@
 #include "mac.h"
 
-Mac::Mac(const std::string r) {
-	unsigned int a, b, c, d, e, f;
-	int res = sscanf(r.c_str(), "%02X:%02X:%02X:%02X:%02X:%02X", &a, &b, &c, &d, &e, &f);
+Mac::Mac(const std::string& r) {
+	std::string s;
+	for(char ch: r) {
+		if ((ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'F') || (ch >= 'a' && ch <= 'f'))
+			s += ch;
+	}
+	int res = sscanf(s.c_str(), "%02hhx%02hhx%02hhx%02hhx%02hhx%02hhx", &mac_[0], &mac_[1], &mac_[2], &mac_[3], &mac_[4], &mac_[5]);
 	if (res != SIZE) {
 		fprintf(stderr, "Mac::Mac sscanf return %d r=%s\n", res, r.c_str());
 		return;
 	}
-	mac_[0] = a;
-	mac_[1] = b;
-	mac_[2] = c;
-	mac_[3] = d;
-	mac_[4] = e;
-	mac_[5] = f;
 }
 
 Mac::operator std::string() const {
-	char buf[32]; // enough size
-	sprintf(buf, "%02x:%02X:%02X:%02X:%02X:%02X",
-		mac_[0],
-		mac_[1],
-		mac_[2],
-		mac_[3],
-		mac_[4],
-		mac_[5]);
+	char buf[20]; // enough size
+	sprintf(buf, "%02x:%02X:%02X:%02X:%02X:%02X", mac_[0], mac_[1], mac_[2], mac_[3], mac_[4], mac_[5]);
 	return std::string(buf);
 }
 
+Mac Mac::randomMac() {
+	Mac res;
+	for (int i = 0; i < SIZE; i++)
+		res.mac_[i] = uint8_t(rand() % 256);
+	res.mac_[0] &= 0x7F;
+	return res;
+}
+
+Mac& Mac::nullMac() {
+	static uint8_t _value[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	static Mac res(_value);
+	return res;
+}
+
+Mac& Mac::broadcastMac() {
+	static uint8_t _value[] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+	static Mac res(_value);
+	return res;
+}
+
+// ----------------------------------------------------------------------------
+// GTEST
+// ----------------------------------------------------------------------------
 #ifdef GTEST
 #include <gtest/gtest.h>
 
+static constexpr uint8_t _temp[] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55};
+
 TEST(Mac, ctorTest) {
-	Mac mac1; // Mac()
+	Mac mac1; // ()
 
-	uint8_t buf[] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55};
-	Mac mac2(buf); // Mac(const uint8_t* r)
+	Mac mac2{mac1}; // (const Mac& rhs)
 
-	Mac mac3("00:11:22:33:44:55"); // Mac(const std::string r)
+	Mac mac3(_temp); // (const uint8_t* rhs)
 
-	EXPECT_EQ(mac2, mac3);
+	Mac mac4(std::string("001122-334455")); // (const std::string& rhs)
+	EXPECT_EQ(mac3, mac4);
+
+	Mac mac5("001122-334455"); // (const std::string& rhs)
+	EXPECT_EQ(mac3, mac5);
 }
 
 TEST(Mac, castingTest) {
-	Mac mac("00:11:22:33:44:55");
+	Mac mac("001122-334455");
 
-	uint8_t buf[Mac::SIZE];
-	memcpy(buf, mac, Mac::SIZE); // operator uint8_t*()
-	EXPECT_TRUE(memcmp(buf, mac, Mac::SIZE) == 0);
+	const uint8_t* uc = (uint8_t*)mac; // operator uint8_t*()
+	uint8_t temp[Mac::SIZE];
+	for (int i = 0; i < Mac::SIZE; i++)
+		temp[i] = *uc++;
+	EXPECT_TRUE(memcmp(&mac, temp, 6) == 0);
 
-	std::string s = std::string(mac); // explicit operator std::string()
-
-	EXPECT_EQ(s, "00:11:22:33:44:55");
+	std::string s2 = std::string(mac); // operator std::string()
+	EXPECT_EQ(s2, "00:11:22:33:44:55");
 }
 
-TEST(Mac, operatorTest) {
-	Mac mac1("00:11:22:33:44:55");
-	Mac mac2("00:11:22:33:44:55");
-	Mac mac3("00:11:22:33:44:56");
+TEST(Mac, funcTest) {
+	Mac mac;
 
-	EXPECT_TRUE(mac1 == mac2);
-	EXPECT_TRUE(mac1 != mac3);
-	EXPECT_TRUE(mac1 < mac3);
+	mac.clear();
+	EXPECT_TRUE(mac.isNull());
+
+	mac = std::string("FF:FF:FF:FF:FF:FF");
+	EXPECT_TRUE(mac.isBroadcast());
+
+	mac = std::string("01:00:5E:00:11:22");
+	EXPECT_TRUE(mac.isMulticast());
+}
+
+#include <map>
+TEST(Mac, mapTest) {
+	typedef std::map<Mac, int> MacMap;
+	MacMap m;
+	m.insert(std::make_pair(Mac("001122-334455"), 1));
+	m.insert(std::make_pair(Mac("001122-334456"), 2));
+	m.insert(std::make_pair(Mac("001122-334457"), 3));
+	EXPECT_EQ(m.size(), 3);
+	MacMap::iterator it = m.begin();
+	EXPECT_EQ(it->second, 1); it++;
+	EXPECT_EQ(it->second, 2); it++;
+	EXPECT_EQ(it->second, 3);
+}
+
+#include <unordered_map>
+TEST(Mac, unordered_mapTest) {
+	typedef std::unordered_map<Mac, int> MacUnorderedMap;
+	MacUnorderedMap m;
+	m.insert(std::make_pair(Mac("001122-334455"), 1));
+	m.insert(std::make_pair(Mac("001122-334456"), 2));
+	m.insert(std::make_pair(Mac("001122-334457"), 3));
+	//EXPECT_EQ(m.size(), 3);
 }
 
 #endif // GTEST
--- src/mac.h
+++ src/mac.h
@@ -4,29 +4,75 @@
 #include <cstring>
 #include <string>
 
+// ----------------------------------------------------------------------------
+// Mac
+// ----------------------------------------------------------------------------
 struct Mac final {
-	static const int SIZE = 6;
+public:
+	static constexpr int SIZE = 6;
 
+protected:
+	uint8_t mac_[SIZE];
+
+public:
 	//
 	// constructor
 	//
 	Mac() {}
-	Mac(const uint8_t* r) { memcpy(this->mac_, r, SIZE); }
-	Mac(const std::string r);
+	Mac(const Mac& rhs) { memcpy(this->mac_, rhs.mac_, SIZE); }
+	Mac(const uint8_t* rhs) { memcpy(this->mac_, rhs, SIZE); }
+	Mac(const std::string& rhs);
+
+	// assign operator
+	Mac& operator = (const Mac& rhs) { memcpy(this->mac_, rhs.mac_, SIZE); return *this; }
 
 	//
 	// casting operator
 	//
-	operator uint8_t*() const { return const_cast<uint8_t*>(mac_); } // default
+	explicit operator uint8_t*() const { return const_cast<uint8_t*>(mac_); }
 	explicit operator std::string() const;
 
 	//
 	// comparison operator
 	//
-	bool operator == (const Mac& r) const { return memcmp(mac_, r.mac_, SIZE) == 0; }
-	bool operator != (const Mac& r) const { return memcmp(mac_, r.mac_, SIZE) != 0; }
-	bool operator < (const Mac& r) const { return memcmp(mac_, r.mac_, SIZE) < 0; }
+	bool operator == (const Mac& rhs) const { return memcmp(mac_, rhs.mac_, SIZE) == 0; }
+	bool operator != (const Mac& rhs) const { return memcmp(mac_, rhs.mac_, SIZE) != 0; }
+	bool operator < (const Mac& rhs) const { return memcmp(mac_, rhs.mac_, SIZE) < 0; }
+	bool operator > (const Mac& rhs) const { return memcmp(mac_, rhs.mac_, SIZE) > 0; }
+	bool operator <= (const Mac& rhs) const { return memcmp(mac_, rhs.mac_, SIZE) <= 0; }
+	bool operator >= (const Mac& rhs) const { return memcmp(mac_, rhs.mac_, SIZE) >= 0; }
+	bool operator == (const uint8_t* rhs) const { return memcmp(mac_, rhs, SIZE) == 0; }
 
-protected:
-	uint8_t mac_[SIZE];
+public:
+	void clear() {
+		*this = nullMac();
+	}
+
+	bool isNull() const {
+		return *this == nullMac();
+	}
+
+	bool isBroadcast() const { // FF:FF:FF:FF:FF:FF
+		return *this == broadcastMac();
+	}
+
+	bool isMulticast() const { // 01:00:5E:0*
+		return mac_[0] == 0x01 && mac_[1] == 0x00 && mac_[2] == 0x5E && (mac_[3] & 0x80) == 0x00;
+	}
+
+	static Mac randomMac();
+	static Mac& nullMac();
+	static Mac& broadcastMac();
 };
+
+namespace std {
+	template<>
+	struct hash<Mac> {
+		size_t operator() (const Mac & rhs) const {
+			uint8_t* p = (uint8_t*)rhs;
+			uint16_t* p1 = (uint16_t*)p;
+			uint32_t* p2 = (uint32_t*)(p + 2);
+			return *p1 + *p2;
+		}
+	};
+}
